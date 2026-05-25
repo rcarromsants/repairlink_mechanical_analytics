1feaa024ad72f365e9395d553f96a4382c2b9961
Rafaela <rcarromeu@oeconnection.com>
2026-05-25 11:50:17 +0100

    fix _repairlink__sources.yml

--- repairlink_mechanical_analytics/models/staging/stg_repairlink__transaction_enu_contacttype.sql
+++ repairlink_mechanical_analytics/models/staging/stg_repairlink__transaction_enu_contacttype.sql
@@ -4,4 +4,4 @@ select
     contacttypeid  as contact_type_id,
     contacttypekey as contact_type_name,
     remark          as contact_type_remark
-from {{ source('repairlink', 'transaction_enu_contacttype') }}
+from {{ source('repairlink', 'TRANSACTION_ENU_CONTACTTYPE') }}
