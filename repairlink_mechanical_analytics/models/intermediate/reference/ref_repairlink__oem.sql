43edeea8fb75856db79e7d50529ea614d50424bc
Rafaela <rcarromeu@oeconnection.com>
2026-05-26 10:42:52 +0100

    standardize OEM names for Chrysler and General Motors

--- repairlink_mechanical_analytics/models/intermediate/reference/ref_repairlink__oem.sql
+++ repairlink_mechanical_analytics/models/intermediate/reference/ref_repairlink__oem.sql
@@ -1,7 +1,29 @@
 {{ config(materialized='table') }}
 
-select distinct
-    oem_id,
-    oem_name
-from {{ ref('stg_repairlink__dealeroemenrollment') }}
-where oem_id is not null
+with source as (
+
+    select distinct
+        oem_id,
+        oem_name
+    from {{ ref('stg_repairlink__dealeroemenrollment') }}
+    where oem_id is not null
+
+),
+
+normalized as (
+
+    select
+        oem_id,
+
+        case
+            when oem_id = 1 then 'Chrysler'
+            when oem_id = 3 then 'General Motors'
+            else oem_name
+        end as oem_name
+
+    from source
+
+)
+
+select distinct *
+from normalized
